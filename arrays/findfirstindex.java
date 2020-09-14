6bf40aa2454764ebd2af2b50d8682da2ef67c2c5
Piyush Anand <piyushanand.1221@gmail.com>
2020-09-14 18:39:30 +0530

    find first and return index

---
+++ arrays/findfirstindex.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class findfirstindex {
+
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+        }
+
+        int value = s.nextInt();
+
+        System.out.println(findfirstindex(arr, value));
+
+    }
+
+    public static int findfirstindex(int[] arr, int value) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
