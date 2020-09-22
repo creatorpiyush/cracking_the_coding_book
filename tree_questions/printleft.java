942982d417e8abe5f4ef3950a35bfa9916db4f42
Piyush Anand <piyushanand.1221@gmail.com>
2020-09-23 00:42:02 +0530

    binary search tree: print left

--- tree_questions/printleft.java
+++ tree_questions/printleft.java
@@ -14,17 +14,25 @@ public class printleft {
     Node root;
 
     public void leftprint() {
-        this.leftprint(this.root);
+        this.leftprint(this.root, 1);
     }
 
-    private void leftprint(Node node) {
-        if (node.left != null) {
-            System.out.print(node.left.data + " ");
+    static int max_level = 0;
+
+    private void leftprint(Node node, int level) {
+
+        if (node == null) {
+            return;
         }
 
-        if (node.left != null) {
-            this.leftprint(node.left);
+        if (max_level < level) {
+            System.out.print(node.data + " ");
+            max_level = level;
         }
+
+        this.leftprint(node.left, level + 1);
+        this.leftprint(node.right, level + 1);
+
     }
 
     public static void main(String[] args) {
